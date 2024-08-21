f0a17be753b6d3caf6bfc7d9144bea0fbb3710b2
Gaurav-1032201414 <96720032+Gaurav-1032201414@users.noreply.github.com>
2024-08-21 17:30:42 +0530

    settings django

--- curlnation/settings.py
+++ curlnation/settings.py
@@ -99,8 +99,12 @@ WSGI_APPLICATION = 'curlnation.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': dj_database_url.config(default=f"postgres://postgres:1601@localhost:5432/postgres"),
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(default=f"postgres://postgres:1601@localhost:5432/postgres"),
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 # Password validation
